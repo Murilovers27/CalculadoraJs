1579af4712243e2d641e301d6912cf15bf2a72ff
cilen <murilopiresandradecruz27@gmail.com>
2025-03-11 10:53:22 -0300

    Logica js

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ function appendNumber(value){
   currentInput += value;
   display.textContent = currentInput;
 }
+
+function appendOperator(operator) {
+  if (currentInput === '' && operator !== '.') return; 
+  currentInput += operator;
+  display.textContent = currentInput;  
+}
+
+
+function calculate(){
+  try{
+      let result = eval(currentInput)
+      if(Number.isInteger(result)){
+        result = result.toFixed(2);
+      }
+      currentInput = result;
+      display.textContent = currentInput;
+
+  }catch(error){
+    display.textContent = "ERROR";
+    currentInput = '';
+
+  }
+}
+
+function clearDisplay(){
+  currentInput = "";
+  display.textContent = currentInput;
+}
